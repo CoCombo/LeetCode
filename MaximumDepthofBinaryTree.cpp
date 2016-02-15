e3f0d16c28280b10e4d910134693f222b19c3ee1
CoCombo <419178356@qq.com>
2016-02-15 10:13:06 +0800

    update

--- MaximumDepthofBinaryTree.cpp
+++ MaximumDepthofBinaryTree.cpp
@@ -11,10 +11,19 @@
  /*My solution time limit exceed*/
 class Solution {
 public:
-    int maxDepth(TreeNode* root) {
+    int maxDepth(TreeNode *root) {
         if(root == nullptr)
             return 0;
         return 1 + (maxDepth(root->left) > maxDepth(root->right) ? 
         			maxDepth(root->left) : maxDepth(root->right));
     }
 };
+/*AC solution*/
+/*I can't understand, I thing it's same to mine*/
+class Solution {
+public:
+	int maxDepth(TreeNode *root) {
+		if(root == nullptr)
+			return 0;
+		return 1 + std::max(maxDepth(root->left), maxDepth(root->right));
+	}
