ce87304e92f6ff8a3c4b2d567cb95d73b496902b
CoCombo <419178356@qq.com>
2016-04-15 14:10:54 +0800

    update

--- 189_RotateArray.cpp
+++ 189_RotateArray.cpp
@@ -11,3 +11,27 @@ public:
         }
     }
 };
+
+//Solution 2
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        if(k == 0 || nums.size() <= 1)
+            return ;
+        
+        k %= nums.size();
+        rotateSub(nums, 0, nums.size() - 1);
+        rotateSub(nums, 0, k - 1);
+        rotateSub(nums, k, nums.size() - 1);
+    }
+    
+    void rotateSub(vector<int>& nums, int left, int right)
+    {
+        while(left < right)
+        {
+            swap(nums[left], nums[right]);
+            left++;
+            right--;
+        }
+    }
+};
